e6179192acbdf7ad7802c8e847efb20e58fb2cd7
eyouyou <393370459@qq.com>
2026-03-24 09:58:29 +0800

    init

--- dist/src/modules/project/project.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectService } from './project.service';
-export declare class ProjectController {
-    private projectService;
-    constructor(projectService: ProjectService);
-    findAll(clubId: string): Promise<$Public.PrismaPromise<T>>;
-    create(body: any): Promise<$Result.GetResult<import(".prisma/client/client").Prisma.$ProjectPayload<ExtArgs>, T, "create", GlobalOmitOptions>>;
-    update(id: string, body: any): Promise<$Result.GetResult<import(".prisma/client/client").Prisma.$ProjectPayload<ExtArgs>, T, "update", GlobalOmitOptions>>;
-}
